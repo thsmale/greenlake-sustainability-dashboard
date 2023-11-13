875be5719de0c90d5888edb782b0f4f0bcc10142
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2023-11-12 21:41:18 -0800

    launch

--- src/global-header/components/BoxComponent.js
+++ src/global-header/components/BoxComponent.js
@@ -1,25 +1,32 @@
 import React from 'react'
 import { Box, Image, ResponsiveContext, Meter } from "grommet";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 const BoxComponent = () => {
+  const percentage = 66;
     return (
       <div>
         <Box fill pad="small" height="1000" background="#F5F5F5">
-         
-                <Meter
-                    
-            values={[
-              {
-                thickness: "large",
-                round: true,
-                value: 60,
-                label: "sixty",
-                type: "circle",
-                onClick: () => {},
-              },
-            ]}
-            aria-label="meter"
+          <CircularProgressbar
+            value={percentage}
+            text={`${percentage}%`}
+            styles={buildStyles({
+              // Rotation of path and trail, in number of turns (0-1)
+              rotation: 0.25,
+              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+              strokeLinecap: "butt",
+              // Text size
+              textSize: "16px",
+              
+              // How long animation takes to go from one percentage to another, in seconds
+              pathTransitionDuration: 0.5,
+              // Colors
+              pathColor: `#01A982`,
+              textColor: "#000",
+              trailColor: "#d6d6d6",
+              backgroundColor: "#01A982",
+            })}
           />
-          <p>Current Usage</p>
+          ;<p>Current Usage</p>
           <p>Predicted Total Usage</p>
         </Box>
       </div>

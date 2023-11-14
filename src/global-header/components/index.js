f3a0149ad984e27ba8e2626e34409d4088a0194c
thsmale <tsmale@gmail.com>
2023-11-13 23:50:13 -0800

    rm more unused files

--- src/global-header/components/index.js
+++ src/global-header/components/index.js
@@ -1,7 +1,5 @@
 export * from './AppIdentity';
 export * from './GlobalHeader';
-export * from './GlobalHeaderExample';
 export * from './HeaderNav';
 export * from './MenuLayer';
-export * from './MockGlobalHeader';
 export * from './UserContext';

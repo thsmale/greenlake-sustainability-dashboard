c4995c256a87a1b27bb8b97a00bf881230e421de
thsmale <tsmale@gmail.com>
2023-11-15 02:26:25 -0800

    rm modal view from about page

--- src/About.js
+++ src/About.js
@@ -1,62 +1,26 @@
 // CenterInformational.js
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Box, Button, Heading, Layer, Paragraph } from 'grommet';
-import { Close } from 'grommet-icons';
+import React from 'react';
+import { Box, Heading, Paragraph } from 'grommet';
 import EmeraldArchitecture from './architecture.png';
 
-const About = ({ containerRef }) => {
-    // containerRef is for demonstration purposes on this site. Most
-    // implementations should likely remove.
-    const [showModal, setShowModal] = useState(false);
+const About = () => {
     return (
         <Box>
-            <Button
-                label="About"
-                onClick={() => setShowModal(true)}
+            <Heading margin="none">
+                HPE Emerald
+            </Heading>
+            <Paragraph margin="none">
+                This is a dashboard that shows real time CPU usage and carbon footprint.
+                It provides suggestions for optimizing cpu and carbon emissions.
+            </Paragraph>
+            <img
+                src={EmeraldArchitecture}
+                alt="Emerald deployment architecture"
+                height="1000"
+                width="1000"
             />
-            {showModal && (
-                <Layer
-                    target={containerRef?.current}
-                    onClickOutside={() => setShowModal(false)}
-                    onEsc={() => setShowModal(false)}
-                    modal={false}
-                    full={true}
-                >
-                    <Box pad="medium">
-                        <Box direction="row" align="start" gap="small">
-                            <Box gap="medium">
-                                <Heading margin="none">
-                                    HPE Emerald
-                                </Heading>
-                                <Paragraph margin="none">
-                                    This is a dashboard that shows real time CPU usage and carbon footprint.
-                                    It provides suggestions for optimizing cpu and carbon emissions.
-                                </Paragraph>
-                                <img 
-                                    src={EmeraldArchitecture}
-                                    alt="Emerald deployment architecture"
-                                    height="1000" 
-                                    width="1000"
-                                />
-                            </Box>
-                            <Button
-                                icon={<Close />}
-                                onClick={() => setShowModal(false)}
-                                a11yTitle={`You are in a layer containing information about HPE Emerald. To close this layer,
-                press Enter.`}
-                                autoFocus
-                            />
-                        </Box>
-                    </Box>
-                </Layer>
-            )}
         </Box>
     );
-};
-
-About.propTypes = {
-    containerRef: PropTypes.object,
 };
 
 export default About;
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,123 +1,14 @@
 // DashboardExample.js
 import React, { useContext, useState } from 'react';
-import { Box, Button, Nav, ResponsiveContext, } from 'grommet';
+import { Box, Button, Nav, ResponsiveContext } from 'grommet';
 import DashboardFooter from './DashboardFooter'
-import Greeting from './Greeting';
-import ServerUsageChart from './Chart';
-import MyLineChart from './LineCharts';
-import ToggleView from './toggle/ToggleView';
-import BoxComponent from './BoxComponent';
-import OptimizeContainer from './OptimizeContainer';
 import GreenlakeHeader from './Header';
 import About from './About';
+import Emerald from './Emerald';
 
 const DashboardExample = () => {
   const size = useContext(ResponsiveContext);
-  const [toggle, togglet] = useState(true);
-  const [predictive, predictiveToggle] = useState(false);
-  const [optimized, setOptimized] = useState(false);
-  const [totalSoFar,setTotalSoFar] = useState(0);
-  const [predictedTotalSoFar, setPredictedTotalSoFar] = useState(0)
-  const [totalEmissons, setTotalEmissons] = useState(0);
-  const [predictedTotalEmissons, setPredictedTotalEmissons] = useState(0)
-  const [transform, setTransform] = useState(false);
-  const toggleTransformFunc = () => {
-    setTransform(!transform);
-    console.log("calling the transform func")
-  }
-  const toggleBtn = () => {
-    togglet(!toggle);
-  };
-  
-  
-  const togglePredictive = () => {
-    console.log("predictive")
-    predictiveToggle(!predictive)
-  }
-
-  const toggleSetOptimized = () => {
-    setOptimized(!optimized)
-    if (predictive) {
-      togglePredictive()
-    }
-  }
-
-  //// start data section 
-function getDatesOfCurrentMonthFormatted() {
-  const dates = [];
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth();
-  const currentYear = currentDate.getFullYear();
-
-  let date = new Date(currentYear, currentMonth, 1);
-
-  while (date.getMonth() === currentMonth) {
-    // Format the date as 'YYYY-MM-DD'
-    const formattedDate = date.toISOString().slice(0, 10);
-    dates.push(formattedDate);
-    date.setDate(date.getDate() + 1);
-  }
-
-  return dates;
-}
-
-const formattedDatesOfCurrentMonth = getDatesOfCurrentMonthFormatted();
-console.log(formattedDatesOfCurrentMonth);
-
-const CurrentDate = () => {
-  return new Date().getDate(); // This gets the current date of the month
-};
-const halfwayIndex = CurrentDate();
-console.log("this is the day of the month");
-
-function generateRandomNumber() {
-  // Generate a random number between 0 (inclusive) and 1 (exclusive)
-  const randomNumber = Math.random();
-
-  // Scale up the number to be between 0 and 29, then floor it to get an integer,
-  // and finally add 1 to make it between 1 and 30
-  return Math.floor(randomNumber * 99) + 1;
-}
-
-// Example usage
-const randomNumber = generateRandomNumber();
-console.log(randomNumber);
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * 20) + 3;
-}
-
-let dataa = [];
-for (let i = 0; i < formattedDatesOfCurrentMonth.length; i++) {
-  let randomNum = generateRandomNumber();
-  let randomEmisson = generateRandomNumber();
-  let randomPredictive = generateRandomNumber();
-  let pre = i + 1 > halfwayIndex ? randomNum - getRandomNumber() : randomNum;
-  let optimizedEmissonVal =
-    i + 1 > halfwayIndex ? randomEmisson - getRandomNumber() : randomEmisson;
-  let temp = {
-    num: i + 1,
-    date: formattedDatesOfCurrentMonth[i],
-    value: randomNum,
-    compute: randomNum,
-    emisson: randomEmisson,
-    optimizedEmissons: optimizedEmissonVal,
-    predictive: pre,
-  };
-  dataa.push(temp);
-}
-
-
-
-/// Optimize Container Logic
-  
-
-
-
-
-//// End Optimize Container Logic   
-
-
+  const [view, setView] = useState('emerald')
 
   return (
     <Box width={{ max: "xxlarge" }} margin="auto" fill>
@@ -130,8 +21,8 @@ for (let i = 0; i < formattedDatesOfCurrentMonth.length; i++) {
           vertical: "small",
         }}
       >
-        <Button label="Emerald" />
-        <About />
+        <Button label="Emerald" onClick={() => setView('emerald')}/>
+        <Button label="About" onClick={ () => setView('about') }/>
       </Nav>
       <Box overflow="auto">
         <Box
@@ -145,51 +36,12 @@ for (let i = 0; i < formattedDatesOfCurrentMonth.length; i++) {
           }}
           flex={false}
         >
-          <Box gap="large">
-            <Greeting />
-          </Box>
-          {/* <ServerUsageChart /> */}
-          <ToggleView toggleBtn={toggleBtn} toggle={toggle} togglet={togglet} />
-          <MyLineChart
-            toggle={toggle}
-            totalSoFar={totalSoFar}
-            setTotalSoFar={setTotalSoFar}
-            predictive={predictive}
-            toggleChart={toggle}
-            predictedTotalSoFar={predictedTotalSoFar}
-            setPredictedTotalSoFar={setPredictedTotalSoFar}
-            totalEmissons={totalEmissons}
-            setTotalEmissons={setTotalEmissons}
-            predictedTotalEmissons={predictedTotalEmissons}
-            setPredictedTotalEmissons={setPredictedTotalEmissons}
-            transform={transform}
-          />
-          <Box direction="row" gap="medium">
-            <Box>
-              <BoxComponent
-                toggle={toggle}
-                totalSoFar={totalSoFar}
-                setTotalSoFar={setTotalSoFar}
-                predictedTotalSoFar={predictedTotalSoFar}
-                setPredictedTotalSoFar={setPredictedTotalSoFar}
-                predictive={predictive}
-                totalEmissons={totalEmissons}
-                setTotalEmissons={setTotalEmissons}
-                predictedTotalEmissons={predictedTotalEmissons}
-                setPredictedTotalEmissons={setPredictedTotalEmissons}
-              />
-            </Box>
-            <OptimizeContainer
-              func={() => {
-                togglePredictive();
-                toggleSetOptimized()
-              }}
-              toggleSetOptimized={toggleSetOptimized}
-              optimized={optimized}
-              setOptimized={setOptimized}
-              toggleTransformFunc={toggleTransformFunc}
-            />
-          </Box>
+          {
+            view === 'emerald' && <Emerald />
+          }
+          {
+            view === 'about' && <About />
+          }
         </Box>
         <DashboardFooter />
       </Box>

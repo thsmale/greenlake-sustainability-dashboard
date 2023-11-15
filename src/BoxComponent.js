fded0fb40db7a6f614b07e62b2394523e02c7232
thsmale <tsmale@gmail.com>
2023-11-14 22:05:17 -0800

    use a card for box component optimize

--- src/BoxComponent.js
+++ src/BoxComponent.js
@@ -1,47 +1,49 @@
 import React from 'react'
-import { Box } from "grommet";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { Card, CardBody, CardHeader } from "grommet";
+import { CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
 const BoxComponent = ({toggle,  predictedTotalEmissons, setPredictedTotalEmissons,totalEmissons,
             setTotalEmissons, predictive, totalSoFar, setTotalSoFar, predictedTotalSoFar }) => {
-  const percentage = 66;
   return (
     <div>
-      <Box fill pad="small" height="1000" background="#F5F5F5">
-        <CircularProgressbar
-          value={percentage}
-          text={`${percentage}%`}
-          styles={buildStyles({
-            // Rotation of path and trail, in number of turns (0-1)
-            rotation: 0.25,
-            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-            strokeLinecap: "butt",
-            // Text size
-            textSize: "16px",
+      <Card>
+        { toggle ? <CardHeader>Compute</CardHeader> : <CardHeader>Emissions</CardHeader> }
+        <CardBody>
+        <CircularProgressbarWithChildren styles={buildStyles({
+              // Rotation of path and trail, in number of turns (0-1)
+              rotation: 0.25,
+              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+              strokeLinecap: "butt",
+              // Text size
+              textSize: "16px",
 
-            // How long animation takes to go from one percentage to another, in seconds
-            pathTransitionDuration: 0.5,
-            // Colors
-            pathColor: `${ toggle ? "#01A982" : "#FF7701"}`,
-            textColor: "#000",
-            trailColor: "#d6d6d6",
-            backgroundColor: "#01A982",
-          })}
-        />
-        <div>
+              // How long animation takes to go from one percentage to another, in seconds
+              pathTransitionDuration: 0.5,
+              // Colors
+              pathColor: `${ toggle ? "#01A982" : "#FF7701"}`,
+              textColor: "#000",
+              trailColor: "#d6d6d6",
+              backgroundColor: "#01A982",
+            })}>
+            <div style={{ fontSize: 48, marginTop: -5 }}>
+              <strong>66%</strong>
+            </div>
+          </CircularProgressbarWithChildren>
+          <div>
+            {toggle ? (
+              <p>Current Usage this month: {totalSoFar} CPU</p>
+            ) : (
+              <p>Current Usage this month: {totalEmissons} MTCO2e</p>
+            )}
+          </div>
           {toggle ? (
-            <p>Current Usage this month: {totalSoFar} CPU</p>
+            <p>Predicted Current Usage this month: {predictedTotalSoFar} CPU</p>
           ) : (
-            <p>Current Usage this month: {totalEmissons} MTCO2e</p>
-          )}
-        </div>
-        {toggle ? (
-          <p>Predicted Current Usage this month: {predictedTotalSoFar} CPU</p>
-        ) : (
-          <p>
-            Predicted Current Usage this month: {predictedTotalEmissons} MTCO2e
-          </p>
+            <p>
+              Predicted Current Usage this month: {predictedTotalEmissons} MTCO2e
+            </p>
         )}
-      </Box>
+        </CardBody>
+      </Card>
     </div>
   );
 };

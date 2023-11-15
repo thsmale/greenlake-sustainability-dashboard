378dc537642eb6b2835c4efcfe48051e8f4cfb9f
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2023-11-14 21:44:29 -0800

    added percentage to circle chart

--- src/BoxComponent.js
+++ src/BoxComponent.js
@@ -4,12 +4,18 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 const BoxComponent = ({toggle,  predictedTotalEmissons, setPredictedTotalEmissons,totalEmissons,
             setTotalEmissons, predictive, totalSoFar, setTotalSoFar, predictedTotalSoFar }) => {
   const percentage = 66;
+  let percentageEmisson = (totalEmissons / predictedTotalEmissons) * 100;
+  let percentageCPU = (totalSoFar / predictedTotalSoFar) * 100;
+  console.log("percentage emisson", percentageEmisson)
+  console.log("percentage cpu", percentageCPU)
   return (
     <div>
       <Box fill pad="small" height="1000" background="#F5F5F5">
         <CircularProgressbar
-          value={percentage}
-          text={`${percentage}%`}
+          value={toggle ? percentageCPU : percentageEmisson}
+          text={`${
+            toggle ? percentageCPU.toFixed(0) : percentageEmisson.toFixed(0)
+          }%`}
           styles={buildStyles({
             // Rotation of path and trail, in number of turns (0-1)
             rotation: 0.25,
@@ -21,7 +27,7 @@ const BoxComponent = ({toggle,  predictedTotalEmissons, setPredictedTotalEmisson
             // How long animation takes to go from one percentage to another, in seconds
             pathTransitionDuration: 0.5,
             // Colors
-            pathColor: `${ toggle ? "#01A982" : "#FF7701"}`,
+            pathColor: `${toggle ? "#01A982" : "#FF7701"}`,
             textColor: "#000",
             trailColor: "#d6d6d6",
             backgroundColor: "#01A982",
